f4d9b608ea554f7ff5a5404f0940906d29c4cf0f
alexandracmihaela <67163119+alexandracmihaela@users.noreply.github.com>
2020-06-30 10:28:54 +0300

    Update interviu.py

--- interviu.py
+++ interviu.py
@@ -2,20 +2,23 @@ import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import json
 import time
+from sense_hat import SenseHat
 
-logfile = "/home/pi/Desktop/Noaje-Alexandra/data.txt"
+sense = SenseHat()
+
+#logfile = "/home/pi/Desktop/Noaje-Alexandra/data.txt"
+logfile = "data.txt"
 
 mHost = "mqtt.beia-telemetrie.ro"
-mTopic = "/training/rpi/alexandra-noaje"
+mTopic = "training/rpi/alexandra-noaje"
 mUser = ""
 mPassword = ""
 
-#channel - port used by sensor
 channel = 15
 
 def local_save(data):
-    file = open(logfile, "w")
-    file.write(data)
+    file = open(logfile, "a+")
+    file.write(data + "\r\n")
     file.close()
 
 def on_connect(client, userdata, flags, rc):
@@ -25,27 +28,33 @@ def on_connect(client, userdata, flags, rc):
         print("Bad connection.")
 
 mqttc = mqtt.Client()
-
 mqttc.username_pw_set(mUser, mPassword)
 mqttc.on_connect = on_connect
 mqttc.connect(mHost, port = 1883)
 mqttc.loop_start()
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(channel, GPIO.IN)
-GPIO.add_event_detect(channel, GPIO.BOTH)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(channel, GPIO.IN)
+#GPIO.add_event_detect(channel, GPIO.BOTH)
 
-
+ 
+  
 def on_detect_sensor(fTopic, fPayload):
-    fClient.publish(fTopic, fPayload)
+    mqttc.publish(fTopic, json.dumps(fPayload))
     
 while True:
-    if GPIO.event_detected(channel):
-        value = GPIO.input(channel)
-        payload = {"SENSOR" : value}
-        save_local(value)
-        on_detect_sensor(mTopic, payload)
-        time.sleep(1) 
-
-    else:
-        print("Nothing has been detected so far...")
+    t = sense.get_temperature()
+    p = sense.get_pressure()
+    h = sense.get_humidity()
+    payload = {"Temperatura:" : t,
+               "Presiune:": p,
+               "Umiditate:": h}
+    message = "Temperature: " + str(t) + " Pressure: " + str(p) + " Humidity: " + str(h)
+    local_save(message)
+    on_detect_sensor(mTopic, payload)
+    
+    print("temperatura" , round(t,3))
+    print("presiune" , round(p,3))
+    print("umiditate" , round(h,3))
+    
+    time.sleep(3)     
